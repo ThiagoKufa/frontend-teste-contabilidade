9962fa44031adb778cdaed51f01160244d92966f
Thiago Kufa <tkufa.code@gmail.com>
2025-09-06 10:54:45 -0300

    refactor: remove unused dependencies and components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 // Barrel exports para facilitar importações
 // Interface Segregation Principle - Exportações específicas e organizadas
 
-export { Title, PrimaryTitle, SecondaryTitle, SmallTitle, MediumTitle, LargeTitle } from './Title';
+export { Title } from './Title';
 export { HeroSection } from './HeroSection';
-export { Skeleton, TextSkeleton, TitleSkeleton, BlockSkeleton } from './Skeleton';
 export { PricingCards } from './PricingCards';
