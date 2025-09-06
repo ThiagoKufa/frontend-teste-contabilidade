00d68cd630c18d4f29eb25cc180b2f8c54feebc4
Thiago Kufa <tkufa.code@gmail.com>
2025-09-06 08:29:51 -0300

    feat: initialize project with React, TypeScript and Vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { HeroSection,  } from './components';
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <HeroSection />
+      <section className='h-dvh'> 
+        section 2
+      </section>
+    </div>
+  );
+}
+
+export default App;
