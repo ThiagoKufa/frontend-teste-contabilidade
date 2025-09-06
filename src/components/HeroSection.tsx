45f01e3f12c39af9a4334773a5f2d45b408834dd
Thiago Kufa <tkufa.code@gmail.com>
2025-09-06 10:35:36 -0300

    feat: replace section with PricingCards component and adjust HeroSection padding

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,7 +20,7 @@ export const HeroSection = ({ className = '' }: HeroSectionProps) => {
   }
 
   return (
-    <section className={`flex flex-col justify-center items-center w-full h-vh py-5 min-w-[1050px] ${className}  bg-white`}>
+    <section className={`flex flex-col justify-center items-center w-full h-vh py-10 min-w-[1050px] ${className}  bg-white`}>
       <Title className="max-w-[640px] mx-auto text-center mb-12 ">
         Por que a <span className="text-primary">Contabilidade.com</span> é a melhor opção em <br/> {city}?
       </Title>
