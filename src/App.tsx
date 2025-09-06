6e07acc408f5d9116cea55c5beea211bc7334462
Thiago Kufa <tkufa.code@gmail.com>
2025-09-06 10:35:47 -0300

    feat: add PricingCards component to replace section 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { HeroSection,  } from './components';
+import { HeroSection, PricingCards } from './components';
 
 function App() {
   return (
     <div className="flex flex-col">
       <HeroSection />
-      <section className='h-dvh'> 
-        section 2
-      </section>
+      <PricingCards />
     </div>
   );
 }
